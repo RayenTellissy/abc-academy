0ad9df5ce2529eba8421b003227d7c9869c4e5b3
RayenTellissy <rayentellissy571@gmail.com>
2023-11-25 03:32:01 +0100

    .

--- src/components/home/components/UpperHome/Slogan.tsx
+++ src/components/home/components/UpperHome/Slogan.tsx
@@ -4,7 +4,7 @@ import { useLocale } from 'next-intl';
 const Slogan = ({ text }: { text: string }) => {
   const locale = useLocale()
 
-  return <p className={`${locale === "ar" ? "text-right" : "text-left"} text-[40px] md:text-[50px] xl:text-[70px] text-navyblue font-bold`}>
+  return <p className={`text-center xl:${locale === "ar" ? "text-end" : "text-start"} text-[40px] md:text-[50px] xl:text-[70px] text-navyblue font-bold`}>
     { text }
   </p>
 };
