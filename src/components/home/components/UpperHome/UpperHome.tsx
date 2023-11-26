2f1995ec832a7a7a8b14b545b450274170df8c25
RayenTellissy <rayentellissy571@gmail.com>
2023-11-26 01:13:25 +0100

    added switching landing page when changing languages

--- src/components/home/components/UpperHome/UpperHome.tsx
+++ src/components/home/components/UpperHome/UpperHome.tsx
@@ -1,73 +1,16 @@
 import React from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
-import { useLocale, useTranslations } from 'next-intl';
-
-// svg
-import HomeLogo from "../../images/home-logo.svg"
-import KidMale from "../../images/kid-male.jpg"
-import KidFemale from "../../images/kid-female.jpg"
-import Teacher from "../../images/teacher.jpg"
+import { useLocale } from 'next-intl';
 
 // components
-import Slogan from './Slogan';
-import Description from './Description';
+import ArabicMain from './components/ArabicMain';
+import NonArabicMain from './components/NonArabicMain';
 
 const UpperHome = () => {
-  const t = useTranslations("home")
   const locale = useLocale()
 
   return (
     <div className='flex flex-row justify-between p-8 md:p-20 gap-3'>
-      <div className='hidden xl:flex flex-row gap-5 flex-shrink-0 h-[350px]'>
-        <div className='flex flex-col gap-6 select-none pointer-events-none'>
-          <div className='flex justify-end'>
-            <Image
-              src={KidMale}
-              alt='male student'
-              className='rounded-[15px] shadow-image'
-              height={150}
-              width={270}
-              placeholder="blur"
-            />
-          </div>
-          <Image src={Teacher} alt='teacher' className='rounded-[15px] shadow-image' height={200} placeholder="blur" />
-        </div>
-        <div className='flex items-center select-none pointer-events-none'>
-          <Image
-            src={KidFemale}
-            alt='female student'
-            className='rounded-[15px] shadow-image'
-            height={250}
-            placeholder="blur"
-          />
-        </div>
-      </div>
-      <div className='flex flex-col gap-5 items-end'>
-        <div className='flex justify-center w-full xl:justify-end pointer-events-none select-none'>
-          <Image src={HomeLogo} alt='home logo' height={100} />
-        </div>
-        <div className='w-full xl:w-[80%]'>
-          <Slogan text={t("slogan")} />
-          <Description text={t("description")} />
-        </div>
-        <div className='w-full justify-center xl:justify-end flex flex-row gap-4 mt-2'>
-          <Link
-            href={`/${locale}/advantages`}
-            className="flex items-center rounded-[7px] border-2 border-orange text-orange text-xl font-bold
-            px-[15px] md:px-[53px] py-3 hover:bg-orange hover:text-white transition-colors duration-200 text-center"
-          >
-            {t("advantages")}
-          </Link>
-          <Link
-            href={`/${locale}/signup`}
-            className="flex items-center rounded-[7px] bg-orange text-white text-xl font-bold
-            px-[15px] md:px-[53px] py-3 hover:bg-orange-hovered transition-colors duration-200 text-center"
-          >
-            {t("signup")}
-          </Link>
-        </div>
-      </div>
+      {locale === "ar" ? <ArabicMain /> : <NonArabicMain />}
     </div>
   );
 };
